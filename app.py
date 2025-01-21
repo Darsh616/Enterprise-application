0d7814deda0d728ad7aadb09ecfad9be8b53afd3
Ubuntu <ubuntu@ip-172-31-11-193.ap-south-1.compute.internal>
2025-01-21 18:30:07 +0000

    Version-2.1

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "<h1>Hello, World!</h1>"
+# MySQL Configuration
+app.config['MYSQL_HOST'] = 'darshan.cj4oqcie8m6x.ap-south-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'admin'  # Replace with your MySQL username
+app.config['MYSQL_PASSWORD'] = 'Local_1234567'  # Replace with your MySQL password
+app.config['MYSQL_DB'] = 'darshan'  # Replace with your database name
 
-if __name__ == "__main__":
-    app.run(debug=True)  # For local testing; Gunicorn will handle deployment.
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM sys_config")  # Query the sys_config table
+    data = cur.fetchall()  # Fetch all data from the table
+    cur.close()
+    return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
